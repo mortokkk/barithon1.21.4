739fd4bb2ada68b73109f4bb4a50b1dcdf3eb4fb
mortoekk <kazakovm337@gmail.com>
2026-08-20 11:43:03 +0300

    Delete barithon/FunTimeBot directory

--- barithon/FunTimeBot/src/main/python/main.py
+++
@@ -1,9 +0,0 @@
-# ========================================
-# ★ ТОЧКА ВХОДА ★
-# ========================================
-
-from bot import FunTimeEndBot
-
-if __name__ == "__main__":
-    bot = FunTimeEndBot()
-    bot.main_loop()
